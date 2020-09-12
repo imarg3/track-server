32150b377ea7f7cd43f07a0a93affbd40cdf6ffb
Arpit Gupta <imarg_3@zoho.com>
2020-09-13 00:36:25 +0530

    User authentication routes

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const User = mongoose.model("User");
+
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = new User({ email, password });
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, "Secret!3");
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: err.message }); // user sent invalid data
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  /**
+   * The HyperText Transfer Protocol (HTTP) 422 Unprocessable Entity response status code indicates that the server understands the content type of the request entity, and the syntax of the request entity is correct, but it was unable to process the contained instructions. We can also use 401 forbidden.
+   */
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must provide email and password" });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "Secret!3");
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
+});
+
+module.exports = router;
